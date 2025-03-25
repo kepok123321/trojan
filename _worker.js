2ffd73215e317c26d25a5e64d7d34eefaaf8977e
herley davidson <kepokasuh1@gmail.com>
2025-03-25 23:15:35 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,14 +1,14 @@
 import { connect } from "cloudflare:sockets";
 
 // Variables
-const rootDomain = "kepokasuh1.workers.dev"; // Ganti dengan domain utama kalian
+const rootDomain = "mbleh-vpn.biz.id"; // Ganti dengan domain utama kalian
 const serviceName = "trojan"; // Ganti dengan nama workers kalian
-const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
-const apiEmail = ""; // Ganti dengan email yang kalian gunakan
-const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
-const zoneID = ""; // Ganti dengan Zone ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
+const apiKey = "6hy_7nSb9UGOKMtmKQd7Ps715ecK8tWEJ-5NIF7x"; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
+const apiEmail = "kepokasuh1@gmail.com"; // Ganti dengan email yang kalian gunakan
+const accountID = "86378c119d311efa003a4bdf83bec804"; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
+const zoneID = "60e7fd8f0f7737cae06c6f2e1e3017d8"; // Ganti dengan Zone ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
 let isApiReady = false;
-let proxyIP = "";
+let proxyIP = "https://raw.githubusercontent.com/FoolVPN-ID/Nautica/refs/heads/main/proxyList.txt";
 let cachedProxyList = [];
 
 // Constant
